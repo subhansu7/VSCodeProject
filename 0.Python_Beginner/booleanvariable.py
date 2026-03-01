36968107a9e46d6b059ca2014ee3edd7dddafee9
subhansu <sarangisubhansu7@gmail.com>
2026-03-01 10:39:19 +0530

    Bool function chek examples added

--- 0.Python_Beginner/booleanvariable.py
+++ 0.Python_Beginner/booleanvariable.py
@@ -2,3 +2,13 @@ a = True
 b = False
 print (a+b)
 print (a)
+
+# bool() function is used to convert a value to a boolean value (True or False) based on the following rules:
+# - The following values are considered False: None, False, 0, 0.0, 0.0, 0j, empty sequences (e.g., '', (), []), and empty mappings (e.g., {}).
+# - All other values are considered True.
+print (bool(0) == False)
+print (bool(1) == True)
+print (bool(-1) == True)
+print (bool("") == False)
+print (bool(" ") == True)
+print (bool() == False)
